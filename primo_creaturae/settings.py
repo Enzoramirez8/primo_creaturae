b67aaa15c56c34edacd3916b152a89bd23eba26e
Enzo <enzoramirez8@gmail.com>
2021-02-05 20:36:43 -0300

    Updated Setting

--- primo_creaturae/settings.py
+++ primo_creaturae/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!j(vg*htfjzo3!_2fz)iotislel4-0xupn##-=fg+7kxgwzasd'
+# SECRET_KEY = '!j(vg*htfjzo3!_2fz)iotislel4-0xupn##-=fg+7kxgwzasd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['primo-creaturae.herokuapp.com']
 
 
 # Application definition
